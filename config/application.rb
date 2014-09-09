b67a6f1eae79f7e0c9679c52575afad34082057e
Stephen Bussey <steve.bussey@salesloft.com>
2014-09-09 02:28:35 -0400

    fix locally

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ Bundler.require(*Rails.groups)
 
 module Bgc
   class Application < Rails::Application
-    config.assets.precompile += %w( .svg .eot .woff .ttf )
     config.assets.paths << Rails.root.join("vendor", "assets", "components")
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   end
 end
