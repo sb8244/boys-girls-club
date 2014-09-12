ab8c10ac5e76174f871a297054b4ebbc88fe399b
Stephen Bussey <steve.bussey@salesloft.com>
2014-09-11 23:28:53 -0400

    donation, social

--- app/assets/javascripts/angular/controllers/donate.js
+++ app/assets/javascripts/angular/controllers/donate.js
@@ -1,3 +1,34 @@
-APP.controller("DonateController", function($scope) {
+APP.controller("DonateController", function($scope, $state) {
   $scope.title = "Donate";
+  $scope.amount = null;
+  $scope.monthly = null;
+  $scope.details = {};
+
+  $scope.setAmount = function(val) {
+    $scope.amount = val;
+    $state.go('donate.monthly');
+  };
+
+  $scope.setMonthly = function(val) {
+    $scope.monthly = val;
+    $state.go('donate.details');
+  };
+
+  $scope.$watch('details', function(details) {
+    if(details.card && details.expiry && details.cvc && details.name) {
+      $scope.canSubmit = true;
+    } else {
+      $scope.canSubmit = false;
+    }
+  }, true);
+
+  $scope.submit = function() {
+    if($scope.canSubmit) {
+      $state.go('donate.finish');
+    }
+  };
+
+  if($state.current.name == "donate.finish" && !$scope.amount) {
+    $state.go('donate.amount');
+  }
 });
